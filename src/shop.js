b884c9ae857826068b41280eb3577a6f3527f053
kwasniew <kwasniewski.mateusz@gmail.com>
2021-10-27 15:14:15 +0200

    backstage pass

--- src/shop.js
+++ src/shop.js
@@ -29,6 +29,21 @@ class Shop {
     // * never decrease in sellIn date
     sulfurasUpdate(item) {}
 
+    // decrease sellIn by 1
+    // never increase quality above 50
+    // drop quality to 0 when after sellIn date
+    // quality increase by 1 over time
+    // if less than or equal 10 days quality increase by 2
+    // if less than or equal 5 days quality increase by 3
+    backstagePassUpdate(item) {
+        item.quality += 1;
+        if(item.sellIn <= 10) item.quality += 1;
+        if(item.sellIn <= 5) item.quality += 1;
+        if(item.quality > 50) item.quality = 50;
+        if(item.sellIn <= 0) item.quality = 0;
+        item.sellIn -= 1;
+    }
+
     updateQuality() {
         this.items.forEach(item => {
 
@@ -40,6 +55,9 @@ class Shop {
             }
             if(item.name === "Sulfuras, Hand of Ragnaros") {
                 return this.sulfurasUpdate(item);
+            }
+            if(item.name === "Backstage passes to a TAFKAL80ETC concert") {
+                return this.backstagePassUpdate(item);
             }
 
             if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
